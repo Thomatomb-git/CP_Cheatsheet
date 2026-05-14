0c1ee9564278570943485010750713e52aa93842
Frederick Kamsono <kamsonofrederick@gmail.com>
2026-05-14 13:10:58 +0700

    Update DS 2

--- source/02_Data Structures/04_Segment Tree Beats.cpp
+++ source/02_Data Structures/04_Segment Tree Beats.cpp
@@ -3,78 +3,78 @@ ll smax[4*N + 5], occ[4*N + 5];//2nd Max, occurence of 1st Max
 ll arr[N + 5];
 
 void pushup(int idx){
-    ST1[idx] = max(ST1[2*idx], ST1[2*idx + 1]);
-    ST2[idx] = ST2[2*idx] + ST2[2*idx + 1];
-    if (ST1[2*idx] == ST1[2*idx + 1]){
-        smax[idx]= max(smax[2*idx], smax[2*idx + 1]);
-        occ[idx] = occ[2*idx] + occ[2*idx + 1];
-    }
-    else{
-        smax[idx] = min(ST1[2*idx], ST1[2*idx + 1]);
-        smax[idx] = max(smax[idx], max(smax[2*idx],smax[2*idx + 1]));
-        if (ST1[2*idx] <= ST1[2*idx + 1])
-            occ[idx] = occ[2*idx + 1];
-        if (ST1[2*idx] > ST1[2*idx + 1])
-            occ[idx]=occ[2*idx];
-    }
+  ST1[idx] = max(ST1[2*idx], ST1[2*idx + 1]);
+  ST2[idx] = ST2[2*idx] + ST2[2*idx + 1];
+  if (ST1[2*idx] == ST1[2*idx + 1]){
+    smax[idx]= max(smax[2*idx], smax[2*idx + 1]);
+    occ[idx] = occ[2*idx] + occ[2*idx + 1];
+  }
+  else{
+    smax[idx] = min(ST1[2*idx], ST1[2*idx + 1]);
+    smax[idx] = max(smax[idx], max(smax[2*idx],smax[2*idx + 1]));
+    if (ST1[2*idx] <= ST1[2*idx + 1])
+      occ[idx] = occ[2*idx + 1];
+    if (ST1[2*idx] > ST1[2*idx + 1])
+      occ[idx]=occ[2*idx];
+  }
 }
 void build(int idx, int l, int r){
-    if (l == r){
-        ST1[idx] = ST2[idx] = arr[l];
-        occ[idx] = 1;
-        smax[idx] = -1;
-        return;
-    }
-    int mid = (l+r)>>1;
-    build(2*idx, l, mid);
-    build(2*idx + 1, mid + 1, r);
-    pushup(idx);
+  if (l == r){
+    ST1[idx] = ST2[idx] = arr[l];
+    occ[idx] = 1;
+    smax[idx] = -1;
+    return;
+  }
+  int mid = (l+r)>>1;
+  build(2*idx, l, mid);
+  build(2*idx + 1, mid + 1, r);
+  pushup(idx);
 }
 void pushdown(int idx, ll v){
-    if(ST1[idx] <= v) return;
-    assert(v > smax[idx]);
-    ST2[idx] -= (ll)(ST1[idx] - v) * (ll)occ[idx];
-    ST1[idx] = v;
+  if(ST1[idx] <= v) return;
+  assert(v > smax[idx]);
+  ST2[idx] -= (ll)(ST1[idx] - v) * (ll)occ[idx];
+  ST1[idx] = v;
 }
 void down(int idx, ll v){
-    pushdown(2*idx, v);
-    pushdown(2*idx + 1, v);
+  pushdown(2*idx, v);
+  pushdown(2*idx + 1, v);
 }
 void update(int idx, int l, int r, int le, int ri, ll v){
-    //update min(Ai,t)
-    if (r < le || ri < l || ST1[idx] <= v)
-        return;
-    if (le <= l && r <= ri && smax[idx] < v){
-        pushdown(idx, v);
-        return;
-    }
-    down(idx, ST1[idx]);
-    int mid = (l+r)>>1;
-    update(2*idx, l, mid, le, ri, v);
-    update(2*idx + 1, mid + 1, r, le, ri, v);
-    pushup(idx);
+  //update min(Ai,t)
+  if (r < le || ri < l || ST1[idx] <= v)
+    return;
+  if (le <= l && r <= ri && smax[idx] < v){
+    pushdown(idx, v);
+    return;
+  }
+  down(idx, ST1[idx]);
+  int mid = (l+r)>>1;
+  update(2*idx, l, mid, le, ri, v);
+  update(2*idx + 1, mid + 1, r, le, ri, v);
+  pushup(idx);
 }
 ll query1(int idx, int l, int r, int le, int ri){
-    //sumquery
-    if(r < le || ri < l) 
-        return -1;
-    if(le <= l && r <= ri) 
-        return ST1[idx];
-    down(idx, ST1[idx]);
-    int mid = (l+r)>>1;
-    ll resa = query1(2*idx, l, mid, le, ri);
-    ll resb = query1(2*idx + 1, mid + 1, r, le, ri);
-    return max(resa, resb);
+  //sumquery
+  if(r < le || ri < l) 
+    return -1;
+  if(le <= l && r <= ri) 
+    return ST1[idx];
+  down(idx, ST1[idx]);
+  int mid = (l+r)>>1;
+  ll resa = query1(2*idx, l, mid, le, ri);
+  ll resb = query1(2*idx + 1, mid + 1, r, le, ri);
+  return max(resa, resb);
 }
 ll query2(int idx, int l, int r, int le, int ri){
-    //maxquery
-    if(r < le || ri < l) 
-        return 0;
-    if(le <= l && r <= ri)
-        return ST2[idx];
-    down(idx, ST1[idx]);
-    int mid = (l+r)>>1;
-    ll resa = query2(2*idx, l, mid, le, ri);
-    ll resb = query2(2*idx + 1, mid + 1, r, le, ri);
-    return resa + resb;
+  //maxquery
+  if(r < le || ri < l) 
+    return 0;
+  if(le <= l && r <= ri)
+    return ST2[idx];
+  down(idx, ST1[idx]);
+  int mid = (l+r)>>1;
+  ll resa = query2(2*idx, l, mid, le, ri);
+  ll resb = query2(2*idx + 1, mid + 1, r, le, ri);
+  return resa + resb;
 }
--- source/02_Data Structures/06_Heavy-Light Decomposition.cpp
+++ source/02_Data Structures/06_Heavy-Light Decomposition.cpp
@@ -1,74 +1,74 @@
 // graph 1-based index
 class HLD{
 private:
-    vector<vector<int>> adj;
-    vector<int> sz, in, out, nxt, par;
-    Segtree DS;
-    int timer = -1;
+  vector<vector<int>> adj;
+  vector<int> sz, in, out, nxt, par;
+  Segtree DS;
+  int timer = -1;
 
-    void dfs (int u, int p){
-        sz[u] = 1;
-        par[u] = p;
-        for (auto &v : adj[u])
-            if (v != p){
-                dfs(v, u);
-                sz[u] += sz[v];
-                if (sz[v] >= sz[adj[u][0]])
-                    swap(v, adj[u][0]);
-            }
-    }
-    void chain(int u, int p){
-        if(p == u) 
-            nxt[u] = u;
-        in[u] = ++timer;
-        for (auto &v : adj[u])
-            if(v != p){
-                nxt[v] = (v == adj[u][0] ? nxt[u] : v);
-                chain(v, u);
-            }
-        out[u] = timer;
-    }
+  void dfs (int u, int p){
+    sz[u] = 1;
+    par[u] = p;
+    for (auto &v : adj[u])
+      if (v != p){
+        dfs(v, u);
+        sz[u] += sz[v];
+        if (sz[v] >= sz[adj[u][0]])
+          swap(v, adj[u][0]);
+      }
+  }
+  void chain(int u, int p){
+    if(p == u) 
+      nxt[u] = u;
+    in[u] = ++timer;
+    for (auto &v : adj[u])
+      if(v != p){
+        nxt[v] = (v == adj[u][0] ? nxt[u] : v);
+        chain(v, u);
+      }
+    out[u] = timer;
+  }
 
 public:
-    HLD(int n){
-        adj.assign(n+1, vector<int>());
-        sz.assign(n+1, 0);
-        in.resize(n+1);
-        out.resize(n+1);
-        nxt.assign(n+1, 0);
-        par.assign(n+1, -1);
+  HLD(int n){
+    adj.assign(n+1, vector<int>());
+    sz.assign(n+1, 0);
+    in.resize(n+1);
+    out.resize(n+1);
+    nxt.assign(n+1, 0);
+    par.assign(n+1, -1);
+  }
+  void add_edge(int u, int v){
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+  void update_path(int u, int v, int x){
+    for(; in[nxt[u]] != in[nxt[v]]; v = par[nxt[v]]){
+      if(in[u] > in[v]) swap(u, v);
+      DS.update(in[nxt[v]], in[v], x);
     }
-    void add_edge(int u, int v){
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+    if(in[u] > in[v]) swap(u, v);
+    DS.update(in[u], in[v], x);
+  }
+  int query_path(int u, int v){
+    int ans = -1;
+    for(; in[nxt[u]] != in[nxt[v]]; v = par[nxt[v]]){
+      if(in[u] > in[v]) swap(u, v);
+      ans = max(ans,DS.query(in[nxt[v]], in[v]));
     }
-    void update_path(int u, int v, int x){
-        for(; in[nxt[u]] != in[nxt[v]]; v = par[nxt[v]]){
-            if(in[u] > in[v]) swap(u, v);
-            DS.update(in[nxt[v]], in[v], x);
-        }
-        if(in[u] > in[v]) swap(u, v);
-        DS.update(in[u], in[v], x);
-    }
-    int query_path(int u, int v){
-        int ans = -1;
-        for(; in[nxt[u]] != in[nxt[v]]; v = par[nxt[v]]){
-            if(in[u] > in[v]) swap(u, v);
-            ans = max(ans,DS.query(in[nxt[v]], in[v]));
-        }
-        if (in[u] > in[v]) swap(u, v);
-        ans = max(ans, DS.query(in[u], in[v]));
-        return ans;
-    }
-    void update_subtree(int u, int x){
-        DS.update(in[u], out[u], x);
-    }
-    int query_subtree(int u){
-        return DS.query(in[u], out[u]);
-    }
-    void init(){
-        dfs(1, 1);
-        chain(1, 1);
-        DS.init(timer + 1);
-    }
+    if (in[u] > in[v]) swap(u, v);
+    ans = max(ans, DS.query(in[u], in[v]));
+    return ans;
+  }
+  void update_subtree(int u, int x){
+    DS.update(in[u], out[u], x);
+  }
+  int query_subtree(int u){
+    return DS.query(in[u], out[u]);
+  }
+  void init(){
+    dfs(1, 1);
+    chain(1, 1);
+    DS.init(timer + 1);
+  }
 };
